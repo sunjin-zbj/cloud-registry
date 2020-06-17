f31a0d9c5fe1e68a776ba4c87a2fc10c8473492b
sunjin991178@163.com <sunjin991178@163.com>
2020-06-17 11:13:35 +0800

    init

---
+++ src/test/java/com/example/cloud/cloudregistry/CloudRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloud.cloudregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
